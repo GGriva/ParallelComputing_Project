b87b6b55120f3c73685bcd6fbee2a985c1948484
GGriva <grivag@rpi.edu>
2018-04-24 11:15:40 -0400

    Delete CTestTestfile.cmake

--- Git Files/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/parallel/2018/PPCgrivag/ROSS/models/ROSS-Models/predator-prey
-# Build directory: /home/parallel/2018/PPCgrivag/ROSS-build/models/ROSS-Models/predator-prey
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
