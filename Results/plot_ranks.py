3682ab472a2deaee0c87805bb59b86c2655cd149
thornb <bthorne3@gmail.com>
2018-04-28 17:05:59 -0400

    fixed labels on x axis

--- Results/plot_ranks.py
+++ Results/plot_ranks.py
@@ -12,15 +12,19 @@ for line in open(f_name):
 	x.append(float(sep[0]))
 	y.append(float(sep[1]))
 
-#plt.ylabel("Event Rate (Events per second)")
+plt.ylabel("Event Rate (Events per second)")
 #plt.ylabel("Execution Time (seconds)")
 #plt.ylabel("Event Rollbacks")
-plt.ylabel("Percent Efficiency (%)")
+#plt.ylabel("Total Rollbacks")
+#plt.ylabel("Percent Efficiency (%)")
 plt.xlabel("MPI ranks")
+
+plt.xticks(x,map(int,x))
 
 plt.plot(x, y, "-o")
 
-plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+
+
 plt.show()
 
 
